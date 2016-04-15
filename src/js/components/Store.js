630b939cea81c025991c169652f183726b5b5ff3
brenopolanski <breno.polanski@gmail.com>
2016-04-14 22:12:01 -0300

    #1 - Add initial code for the workshop

---
+++ src/js/components/Store.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var React    = require('react');
+var Grid     = require('react-bootstrap/lib/Grid');
+var Header   = require('./Header');
+var Cart     = require('./Cart');
+var Products = require('./Products');
+var Footer   = require('./Footer');
+
+var Store = React.createClass({
+  getInitialState: function() {
+    return {
+      beers: {},
+      cart: {}
+    }
+  },
+
+  componentWillMount: function() {
+    this.loadBeers();
+  },
+
+  componentDidMount: function() {
+    var localStorageData = localStorage.getItem('cart');
+
+    if (localStorageData) {
+      this.setState({
+        cart: JSON.parse(localStorageData)
+      });
+    }
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+    localStorage.setItem('cart', JSON.stringify(nextState.cart));
+  },
+
+  loadBeers: function() {
+    this.setState({
+      beers: require('../utils/data-beers')
+    });
+  },
+
+  addToCart: function(key, amount) {
+    this.state.cart[key] = this.state.cart[key] + amount || amount;
+    this.setState({
+      cart: this.state.cart
+    });
+  },
+
+  removeFromCart: function(key) {
+    delete this.state.cart[key];
+    this.setState({
+      cart: this.state.cart
+    });
+  },
+
+  render: function() {
+    return (
+      <Grid>
+        <Header />
+        <Cart
+          beers={this.state.beers}
+          cart={this.state.cart}
+          removeFromCart={this.removeFromCart}
+        />
+        <Products beers={this.state.beers} addToCart={this.addToCart} />
+        <Footer />
+      </Grid>
+    );
+  }
+});
+
+module.exports = Store;
